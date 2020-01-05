1e36152c426544c35d03de7e5082e98626bb9daa
Paulo Jesus <paulohenrique.nj@gmail.com>
2020-01-05 10:01:11 -0300

    Alterando mensagem

--- scripts/send_notification.py
+++ scripts/send_notification.py
@@ -5,12 +5,12 @@ import subprocess
 
 
 if len(sys.argv) == 3:
-    sleep_time = int(sys.argv[1]) * 1 
+    sleep_time = int(sys.argv[1]) * 60
     message = sys.argv[2]
     cmd = ' nohup sleep {}; notify-send "{}"'.format(str(sleep_time), message)
 
     subprocess.Popen(cmd, shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
 
 else:
-    print('O primeiro parametro é minutos o segundo é a mensagem ')
+    print('O primeiro parametro quantidade minutos o segundo é a mensagem.')
 
